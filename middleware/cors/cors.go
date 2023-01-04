bbb4b24a83c6798f24d32826fbf624edd879b641
maczh <Micro!070804>
2023-01-04 11:05:38 +0800

    垮域增加自定义标头支持

--- middleware/cors/cors.go
+++ middleware/cors/cors.go
@@ -1,16 +1,21 @@
 package cors
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strings"
 )
 
-func Cors() gin.HandlerFunc {
+func Cors(headers ...string) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		method := c.Request.Method
-
+		allowHeaders := "Content-Type,AccessToken,X-CSRF-Token, Authorization, Token"
+		if len(headers) > 0 {
+			allowHeaders = fmt.Sprintf("%s, %s", allowHeaders, strings.Join(headers, ","))
+		}
 		c.Header("Access-Control-Allow-Origin", "*")
-		c.Header("Access-Control-Allow-Headers", "Content-Type,AccessToken,X-CSRF-Token, Authorization, Token")
+		c.Header("Access-Control-Allow-Headers", allowHeaders)
 		c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
 		c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type")
 		c.Header("Access-Control-Allow-Credentials", "true")
