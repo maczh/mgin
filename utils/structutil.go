0625c6a46437a878c00cedd2075339b012e4c8b0
maczh <Micro!070804>
2023-02-02 20:17:40 +0800

    add: Struct2MapString函数

--- utils/structutil.go
+++ utils/structutil.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"errors"
+	"fmt"
 	"reflect"
 )
 
@@ -42,7 +43,6 @@ func StructJsonTagToMap(obj interface{}) map[string]interface{} {
 
 // Struct2Map return map
 func Struct2Map(obj interface{}) map[string]interface{} {
-	var node map[string]interface{}
 	objT := reflect.TypeOf(obj)
 	if objT.Kind() != reflect.Struct {
 		panic(errors.New("argument is not of the expected type"))
@@ -52,20 +52,41 @@ func Struct2Map(obj interface{}) map[string]interface{} {
 	for i := 0; i < objT.NumField(); i++ {
 		switch objV.Field(i).Type().Kind() {
 		case reflect.Struct:
-			node = Struct2Map(objV.Field(i).Interface())
+			node := Struct2Map(objV.Field(i).Interface())
 			data[objT.Field(i).Name] = node
 		case reflect.Slice:
 			target := objV.Field(i).Interface()
 			tmp := make([]map[string]interface{}, reflect.ValueOf(target).Len())
 			for j := 0; j < reflect.ValueOf(target).Len(); j++ {
 				if reflect.ValueOf(target).Index(j).Kind() == reflect.Struct {
-					node = Struct2Map(reflect.ValueOf(target).Index(j).Interface())
+					node := Struct2Map(reflect.ValueOf(target).Index(j).Interface())
 					tmp[j] = node
 				}
 			}
 			data[objT.Field(i).Name] = tmp
 		default:
 			data[objT.Field(i).Name] = objV.Field(i).Interface()
+		}
+	}
+	return data
+}
+
+func Struct2MapString(obj interface{}) map[string]string {
+	objT := reflect.TypeOf(obj)
+	if objT.Kind() != reflect.Struct {
+		panic(errors.New("argument is not of the expected type"))
+	}
+	objV := reflect.ValueOf(obj)
+	var data = make(map[string]string)
+	for i := 0; i < objT.NumField(); i++ {
+		switch objV.Field(i).Type().Kind() {
+		case reflect.Struct, reflect.Slice, reflect.Map:
+			val := ToJSON(objV.Field(i).Interface())
+			data[objT.Field(i).Name] = val
+		case reflect.String:
+			data[objT.Field(i).Name] = objV.Field(i).String()
+		default:
+			data[objT.Field(i).Name] = fmt.Sprintf("%v", objV.Field(i).Interface())
 		}
 	}
 	return data
