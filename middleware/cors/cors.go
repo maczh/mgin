c7e089d80296653b020a06b3e1b369147ab7cf77
Macro.James <maczh@qq.com>
2025-12-16 19:40:25 +0800

    允许外部配置跨域处理

--- middleware/cors/cors.go
+++ middleware/cors/cors.go
@@ -10,13 +10,18 @@ import (
 func Cors(headers ...string) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		method := c.Request.Method
-		allowHeaders := "Content-Type,AccessToken,X-CSRF-Token, Authorization, Token"
-		if len(headers) > 0 {
+		allowHeaders := "Content-Type,AccessToken,X-CSRF-Token,Authorization,Token"
+		if len(headers) > 0 && headers[0] != "" {
 			allowHeaders = fmt.Sprintf("%s, %s", allowHeaders, strings.Join(headers, ","))
 		}
-		c.Header("Access-Control-Allow-Origin", "*")
+		origin := c.Request.Header.Get("Origin")
+		if origin != "" {
+			c.Header("Access-Control-Allow-Origin", origin)
+		} else {
+			c.Header("Access-Control-Allow-Origin", "*")
+		}
 		c.Header("Access-Control-Allow-Headers", allowHeaders)
-		c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+		c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, PATCH, HEAD, UPDATE")
 		c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type")
 		c.Header("Access-Control-Allow-Credentials", "true")
 
